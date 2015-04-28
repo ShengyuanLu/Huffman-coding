fecd3613bc0863df46f05043f24be79f9cd7c815
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-28 22:21:02 +0800

    Add HuffmanTreeTest

---
+++ src/test/scala/HuffmanTreeTest.scala
@@ -0,0 +1,28 @@
+import com.google.common.collect.Sets
+import org.junit.Assert._
+import org.junit.Test
+
+class HuffmanTreeTest {
+  @Test
+  def testAB {
+    val nodes = List(Node("A", 2),  Node("B", 3))
+    val tree: Node = HuffmanTree.buildHuffmanTree(nodes)
+    assertEquals(5, tree.sum)
+  }
+
+  @Test
+  def testABC {
+    val nodes = List(Node("A", 2), Node("B", 3), Node("C", 4))
+    val tree: Node = HuffmanTree.buildHuffmanTree(nodes)
+    System.out.println(tree)
+    assertEquals(14, tree.sum)
+  }
+
+  @Test
+  def testComplex {
+    val nodes = List(Node("A", 5), Node("B", 2), Node("C", 4), Node("D", 7), new Node("E", 9))
+    val tree: Node = HuffmanTree.buildHuffmanTree(nodes)
+    assertEquals(60, tree.sum)
+  }
+
+}
